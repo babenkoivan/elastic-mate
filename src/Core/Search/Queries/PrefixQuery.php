3ce77ff8ff204c5eabf7104fe5f756415775f315
ivanbabenko <babenko.i.a@gmail.com>
2018-09-24 22:56:22 +0200

    Prefix query

---
+++ src/Core/Search/Queries/PrefixQuery.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace BabenkoIvan\ElasticMate\Core\Search\Queries;
+
+use BabenkoIvan\ElasticMate\Core\Contracts\Search\Queries\Query;
+
+final class PrefixQuery implements Query
+{
+    /**
+     * @var string
+     */
+    private $field;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * @var float|null
+     */
+    private $boost;
+
+    /**
+     * @param string $field
+     * @param string $value
+     * @param float|null $boost
+     */
+    public function __construct(string $field, string $value, float $boost = null)
+    {
+        $this->field = $field;
+        $this->value = $value;
+        $this->boost = $boost;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray(): array
+    {
+        $query = [
+            'value' => $this->value
+        ];
+
+        if (isset($this->boost)) {
+            $query['boost'] = $this->boost;
+        }
+
+        return [
+            'prefix' => [
+                $this->field => $query
+            ]
+        ];
+    }
+}
---
+++ tests/Core/Search/Queries/PrefixQueryTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace BabenkoIvan\ElasticMate\Core\Search\Queries;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \BabenkoIvan\ElasticMate\Core\Search\Queries\PrefixQuery
+ */
+final class PrefixQueryTest extends TestCase
+{
+    public function test_prefix_query_can_be_converted_to_array(): void
+    {
+        $prefixQuery = new PrefixQuery('foo', 'ba', 1.7);
+
+        $this->assertSame(
+            [
+                'prefix' => [
+                    'foo' => [
+                        'value' => 'ba',
+                        'boost' => 1.7
+                    ]
+                ]
+            ],
+            $prefixQuery->toArray()
+        );
+    }
+}
