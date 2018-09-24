16c2b0956b7502ea785f1b33f749ffe6e73070be
ivanbabenko <babenko.i.a@gmail.com>
2018-09-24 23:07:32 +0200

    Wildcard query

---
+++ src/Core/Search/Queries/WildcardQuery.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace BabenkoIvan\ElasticMate\Core\Search\Queries;
+
+use BabenkoIvan\ElasticMate\Core\Contracts\Search\Queries\Query;
+
+final class WildcardQuery implements Query
+{
+    /**
+     * @var string
+     */
+    private $field;
+
+    /**
+     * @var string
+     */
+    private $wildcard;
+
+    /**
+     * @var float|null
+     */
+    private $boost;
+
+    /**
+     * @param string $field
+     * @param string $wildcard
+     * @param float|null $boost
+     */
+    public function __construct(string $field, string $wildcard, float $boost = null)
+    {
+        $this->field = $field;
+        $this->wildcard = $wildcard;
+        $this->boost = $boost;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray(): array
+    {
+        $query = [
+            'wildcard' => $this->wildcard
+        ];
+
+        if (isset($this->boost)) {
+            $query['boost'] = $this->boost;
+        }
+
+        return [
+            'wildcard' => [
+                $this->field => $query
+            ]
+        ];
+    }
+}
---
+++ tests/Core/Search/Queries/WildcardQueryTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace BabenkoIvan\ElasticMate\Core\Search\Queries;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \BabenkoIvan\ElasticMate\Core\Search\Queries\WildcardQuery
+ */
+final class WildcardQueryTest extends TestCase
+{
+    public function test_wildcard_query_can_be_converted_to_array(): void
+    {
+        $wildcardQuery = new WildcardQuery('foo', 'b*r', 1.3);
+
+        $this->assertSame(
+            [
+                'wildcard' => [
+                    'foo' => [
+                        'wildcard' => 'b*r',
+                        'boost' => 1.3
+                    ]
+                ]
+            ],
+            $wildcardQuery->toArray()
+        );
+    }
+}
