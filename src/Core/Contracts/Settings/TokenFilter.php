f928ff9efe990a7e9aaeffcc481e8b976b1142b3
ivanbabenko <babenko.i.a@gmail.com>
2018-10-15 19:34:06 +0200

    Standard token filter

---
+++ src/Core/Contracts/Settings/TokenFilter.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace BabenkoIvan\ElasticMate\Core\Contracts\Settings;
+
+use BabenkoIvan\ElasticMate\Core\Contracts\Arrayable;
+use BabenkoIvan\ElasticMate\Core\Contracts\Nameable;
+
+interface TokenFilter extends Nameable, Arrayable
+{
+    const TYPE_STANDARD = 'standard';
+}
---
+++ src/Core/Settings/TokenFilters/AbstractTokenFilter.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace BabenkoIvan\ElasticMate\Core\Settings\TokenFilters;
+
+use BabenkoIvan\ElasticMate\Core\Contracts\Settings\TokenFilter;
+use BabenkoIvan\ElasticMate\Core\Traits\HasName;
+
+abstract class AbstractTokenFilter implements TokenFilter
+{
+    use HasName;
+
+    /**
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    abstract public function toArray(): array;
+}
