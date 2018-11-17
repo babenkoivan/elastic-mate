ea2576c0c7c27ad1a0a968da1621f75a624fb75e
ivanbabenko <babenko.i.a@gmail.com>
2018-11-17 13:45:52 +0100

    Date property creation test

--- tests/Core/Mapping/Properties/DatePropertyTest.php
+++ tests/Core/Mapping/Properties/DatePropertyTest.php
@@ -3,15 +3,28 @@ declare(strict_types=1);
 
 namespace BabenkoIvan\ElasticMate\Core\Mapping\Properties;
 
+use BabenkoIvan\ElasticMate\Core\Entities\Index;
+use BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager;
 use BabenkoIvan\ElasticMate\Core\Mapping\Mapping;
+use BabenkoIvan\ElasticMate\Traits\HasClient;
+use BabenkoIvan\ElasticMate\Traits\HasMappingAssertions;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \BabenkoIvan\ElasticMate\Core\Mapping\Properties\DateProperty
  * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\AbstractProperty
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Client
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\ClientFactory
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Namespaces\IndicesNamespace
+ * @uses   \BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager
+ * @uses   \BabenkoIvan\ElasticMate\Core\Settings\Settings
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Mapping
+ * @uses   \BabenkoIvan\ElasticMate\Core\Entities\Index
  */
 final class DatePropertyTest extends TestCase
 {
+    use HasClient, HasMappingAssertions;
+
     public function test_date_property_has_correct_default_values(): void
     {
         $dateProperty = new DateProperty('foo');
@@ -57,5 +70,32 @@ final class DatePropertyTest extends TestCase
             ],
             $dateProperty->toArray()
         );
+    }
+
+    public function test_date_property_can_be_created(): void
+    {
+        $mapping = (new Mapping())
+            ->addProperty(
+                new DateProperty('foo')
+            )
+            ->addProperty(
+                (new DateProperty('bar'))
+                    ->setBoost(1.2)
+                    ->setDocValues(false)
+                    ->setFormat('yyyy-MM-dd HH:mm:ss')
+                    ->setLocale('en')
+                    ->setIgnoreMalformed(true)
+                    ->setIndex(false)
+                    ->setNullValue('NULL')
+                    ->setStore(true)
+            );
+
+        $index = (new Index('test'))
+            ->setMapping($mapping);
+
+        $indexManager = new IndexManager($this->client);
+        $indexManager->create($index);
+
+        $this->assertMappingMatch($mapping->toArray(), $this->getIndexMapping($index->getName()));
     }
 }
