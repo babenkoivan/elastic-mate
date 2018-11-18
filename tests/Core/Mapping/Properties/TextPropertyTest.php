a8f2d60d4c56c2aad3553178b5625a837a3a6561
ivanbabenko <babenko.i.a@gmail.com>
2018-11-18 11:41:58 +0100

    Text property creation test

--- tests/Core/Mapping/Properties/TextPropertyTest.php
+++ tests/Core/Mapping/Properties/TextPropertyTest.php
@@ -3,15 +3,28 @@ declare(strict_types=1);
 
 namespace BabenkoIvan\ElasticMate\Core\Mapping;
 
+use BabenkoIvan\ElasticMate\Core\Entities\Index;
+use BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager;
 use BabenkoIvan\ElasticMate\Core\Mapping\Properties\TextProperty;
+use BabenkoIvan\ElasticMate\Traits\HasClient;
+use BabenkoIvan\ElasticMate\Traits\HasMappingAssertions;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \BabenkoIvan\ElasticMate\Core\Mapping\Properties\TextProperty
  * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\AbstractProperty
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Client
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\ClientFactory
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Namespaces\IndicesNamespace
+ * @uses   \BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager
+ * @uses   \BabenkoIvan\ElasticMate\Core\Settings\Settings
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Mapping
+ * @uses   \BabenkoIvan\ElasticMate\Core\Entities\Index
  */
 final class TextPropertyTest extends TestCase
 {
+    use HasClient, HasMappingAssertions;
+
     public function test_text_property_has_correct_default_values(): void
     {
         $textProperty = new TextProperty('foo');
@@ -67,5 +80,36 @@ final class TextPropertyTest extends TestCase
             ],
             $textProperty->toArray()
         );
+    }
+
+    public function test_text_property_can_be_created(): void
+    {
+        $mapping = (new Mapping())
+            ->addProperty(
+                new TextProperty('foo')
+            )
+            ->addProperty(
+                (new TextProperty('bar'))
+                    ->setBoost(1.7)
+                    ->setEagerGlobalOrdinals(false)
+                    ->setFieldData(true)
+                    ->setIndex(false)
+                    ->setIndexOptions(Mapping::INDEX_OPTIONS_DOCS)
+                    ->setNorms(true)
+                    ->setStore(true)
+                    ->setSimilarity(Mapping::SIMILARITY_BOOLEAN)
+                    ->setTermVector(Mapping::TERM_VECTOR_WITH_POSITIONS_AND_OFFSETS)
+                    ->setAnalyzer('whitespace')
+                    ->setSearchAnalyzer('standard')
+                    ->setSearchQuoteAnalyzer('simple')
+            );
+
+        $index = (new Index('test'))
+            ->setMapping($mapping);
+
+        $indexManager = new IndexManager($this->client);
+        $indexManager->create($index);
+
+        $this->assertMappingMatch($mapping->toArray(), $this->getIndexMapping($index->getName()));
     }
 }
