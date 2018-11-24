6fe69f1a758253f797b44d62a67e53900e13f82c
ivanbabenko <babenko.i.a@gmail.com>
2018-11-24 10:38:27 +0100

    Match all query integration test

--- tests/Core/Search/Queries/MatchAllQueryTest.php
+++ tests/Core/Search/Queries/MatchAllQueryTest.php
@@ -3,14 +3,45 @@ declare(strict_types=1);
 
 namespace BabenkoIvan\ElasticMate\Core\Search\Queries;
 
+use BabenkoIvan\ElasticMate\Core\Content\Content;
+use BabenkoIvan\ElasticMate\Core\Entities\Document;
+use BabenkoIvan\ElasticMate\Core\Entities\Index;
+use BabenkoIvan\ElasticMate\Core\EntityManagers\BulkDocumentManager;
+use BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager;
+use BabenkoIvan\ElasticMate\Core\Search\Request;
+use BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\FieldSort;
+use BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\SimpleSort;
+use BabenkoIvan\ElasticMate\Traits\HasClient;
 use PHPUnit\Framework\TestCase;
 use stdClass;
 
 /**
  * @covers \BabenkoIvan\ElasticMate\Core\Search\Queries\MatchAllQuery
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Client
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\ClientFactory
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Namespaces\IndicesNamespace
+ * @uses   \BabenkoIvan\ElasticMate\Core\Content\Content
+ * @uses   \BabenkoIvan\ElasticMate\Core\Content\Mutators\ContentMutator
+ * @uses   \BabenkoIvan\ElasticMate\Core\Entities\Document
+ * @uses   \BabenkoIvan\ElasticMate\Core\Entities\Index
+ * @uses   \BabenkoIvan\ElasticMate\Core\EntityManagers\BulkDocumentManager
+ * @uses   \BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager
+ * @uses   \BabenkoIvan\ElasticMate\Core\Settings\Settings
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Mapping
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\AbstractProperty
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\AbstractNumericProperty
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\IntegerNumericProperty
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\KeywordProperty
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\FieldSort
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\SimpleSort
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Request
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Response
+ * @uses   \Illuminate\Support\Collection
  */
 final class MatchAllQueryTest extends TestCase
 {
+    use HasClient;
+
     public function test_match_all_query_can_be_converted_to_array(): void
     {
         $matchAllQuery = new MatchAllQuery();
@@ -20,6 +51,35 @@ final class MatchAllQueryTest extends TestCase
                 'match_all' => new stdClass()
             ],
             $matchAllQuery->toArray()
+        );
+    }
+
+    public function test_match_all_query_retrieves_relevant_documents_from_index(): void
+    {
+        $index = new Index('test');
+
+        $documents = collect([
+            new Document('1', new Content(['integerField' => 1, 'stringField' => 'foo'])),
+            new Document('2', new Content(['integerField' => 2, 'stringField' => 'bar']))
+        ]);
+
+        $indexManager = new IndexManager($this->client);
+        $documentManager = new BulkDocumentManager($this->client);
+
+        $indexManager->create($index);
+        $documentManager->index($index, $documents, true);
+
+        $query = new MatchAllQuery();
+        $sort = new SimpleSort(collect([new FieldSort('_id', FieldSort::ORDER_ASC)]));
+
+        $request = (new Request($query))
+            ->setSort($sort);
+
+        $response = $documentManager->search($index, $request);
+
+        $this->assertEquals(
+            $documents,
+            $response->getDocuments()
         );
     }
 }
