6d4af44af5ee2f3014d391482d40414e2eb71033
ivanbabenko <babenko.i.a@gmail.com>
2018-11-30 11:07:06 +0100

    Wildcard query integration test

--- tests/Core/Search/Queries/WildcardQueryTest.php
+++ tests/Core/Search/Queries/WildcardQueryTest.php
@@ -3,13 +3,45 @@ declare(strict_types=1);
 
 namespace BabenkoIvan\ElasticMate\Core\Search\Queries;
 
+use BabenkoIvan\ElasticMate\Core\Content\Content;
+use BabenkoIvan\ElasticMate\Core\Entities\Document;
+use BabenkoIvan\ElasticMate\Core\Entities\Index;
+use BabenkoIvan\ElasticMate\Core\EntityManagers\BulkDocumentManager;
+use BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager;
+use BabenkoIvan\ElasticMate\Core\Mapping\Mapping;
+use BabenkoIvan\ElasticMate\Core\Mapping\Properties\TextProperty;
+use BabenkoIvan\ElasticMate\Core\Search\Request;
+use BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\FieldSort;
+use BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\SimpleSort;
+use BabenkoIvan\ElasticMate\Traits\HasClient;
+use Illuminate\Support\Collection;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \BabenkoIvan\ElasticMate\Core\Search\Queries\WildcardQuery
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Client
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\ClientFactory
+ * @uses   \BabenkoIvan\ElasticMate\Infrastructure\Client\Namespaces\IndicesNamespace
+ * @uses   \BabenkoIvan\ElasticMate\Core\Content\Content
+ * @uses   \BabenkoIvan\ElasticMate\Core\Content\Mutators\ContentMutator
+ * @uses   \BabenkoIvan\ElasticMate\Core\Entities\Document
+ * @uses   \BabenkoIvan\ElasticMate\Core\Entities\Index
+ * @uses   \BabenkoIvan\ElasticMate\Core\EntityManagers\BulkDocumentManager
+ * @uses   \BabenkoIvan\ElasticMate\Core\EntityManagers\IndexManager
+ * @uses   \BabenkoIvan\ElasticMate\Core\Settings\Settings
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Mapping
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\AbstractProperty
+ * @uses   \BabenkoIvan\ElasticMate\Core\Mapping\Properties\TextProperty
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\FieldSort
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Sort\Simple\SimpleSort
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Request
+ * @uses   \BabenkoIvan\ElasticMate\Core\Search\Response
+ * @uses   \Illuminate\Support\Collection
  */
 final class WildcardQueryTest extends TestCase
 {
+    use HasClient;
+
     public function test_wildcard_query_can_be_converted_to_array(): void
     {
         $wildcardQuery = (new WildcardQuery('foo', 'b*r'))->setBoost(1.3);
@@ -24,6 +56,73 @@ final class WildcardQueryTest extends TestCase
                 ]
             ],
             $wildcardQuery->toArray()
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function wildcardQueryProvider(): array
+    {
+        return [
+            [
+                'stringField',
+                'f*',
+                collect([
+                    new Document('1', new Content(['stringField' => 'foo']))
+                ])
+            ],
+            [
+                'stringField',
+                'b*r',
+                collect([
+                    new Document('2', new Content(['stringField' => 'bar']))
+                ])
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider wildcardQueryProvider
+     * @testdox Wildcard query "$field = $wildcard" retrieves relevant documents from index
+     *
+     * @param string $field
+     * @param string $wildcard
+     * @param Collection $expectedDocuments
+     */
+    public function test_wildcard_query_retrieves_relevant_documents_from_index(
+        string $field,
+        string $wildcard,
+        Collection $expectedDocuments
+    ): void {
+        $mapping = (new Mapping())
+            ->addProperty(new TextProperty('stringField'));
+
+        $index = (new Index('test'))
+            ->setMapping($mapping);
+
+        $documents = collect([
+            new Document('1', new Content(['stringField' => 'foo'])),
+            new Document('2', new Content(['stringField' => 'bar']))
+        ]);
+
+        $indexManager = new IndexManager($this->client);
+        $documentManager = new BulkDocumentManager($this->client);
+
+        $indexManager->create($index);
+        $documentManager->index($index, $documents, true);
+
+        $query = new WildcardQuery($field, $wildcard);
+        $sort = new SimpleSort(collect([new FieldSort('_id', FieldSort::ORDER_ASC)]));
+
+        $request = (new Request($query))
+            ->setSort($sort);
+
+        $response = $documentManager->search($index, $request);
+
+        $this->assertEquals(
+            $expectedDocuments,
+            $response->getDocuments()
         );
     }
 }
